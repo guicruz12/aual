a712dc13c58d4eea50a379872e2390b3d8300fda
guicruz12 <130380751+guicruz12@users.noreply.github.com>
2023-09-04 11:46:19 -0300

    Add files via upload

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,8 @@
+//busca no site pelo titulo e armazena NA VARIAVEL
+let titulo = document.querySelector(".titulo");
+
+//MOSTRA NA DEVTOOL O CONTEUDO DA VARIAVEL 
+console.log(titulo.textContent);
+
+//altera o conteudo da variavel
+titulo.textContent = "Pica Grande Nutrição"

c9eb29c94ab72b7d2147ed20d0e0cc1f02fae96c
guicruz12 <130380751+guicruz12@users.noreply.github.com>
2023-09-20 11:54:38 -0300

    Delete introducao-javascript directory

--- introducao-javascript/js/principal.js
+++
@@ -1,8 +0,0 @@
-//busca no site pelo titulo e armazena NA VARIAVEL
-let titulo = document.querySelector(".titulo");
-
-//MOSTRA NA DEVTOOL O CONTEUDO DA VARIAVEL 
-console.log(titulo.textContent);
-
-//altera o conteudo da variavel
-titulo.textContent = "Pica Grande Nutrição"
